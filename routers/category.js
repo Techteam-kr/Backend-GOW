708391e4211dc8adbbe8bcdc310df20a1cc7ce2b
Techteam-kr <Techteam.kr@gmail.com>
2022-04-11 17:27:43 +0530

    category-api response changed

--- routers/category.js
+++ routers/category.js
@@ -4,9 +4,21 @@ const data = require('../masterJson')
 
 router.get('/', async(req,res) => {
 
-    const CategoryObject = [{
-        "categories" : ["Housing","Pension","Disability","Transgender","Women & Children","Health","Unorganised Labour","Students","Skill Development","BPL","Other"]
-    }]
+    const CategoryObject = {
+        "categories": [
+            "Housing",
+            "Pension",
+            "Disability",
+            "Transgender",
+            "Women & Children",
+            "Health",
+            "Unorganised Labour",
+            "Students",
+            "Skill Development",
+            "BPL Card Holder",
+            "Other"
+        ]
+    }
     console.log("Request Recieved")
     res.status(201).send(CategoryObject)
 })
