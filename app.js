94deb8fb13b34c27299b5240c56dd6fa5eb7d924
Techteam-kr <Techteam.kr@gmail.com>
2022-04-11 17:01:48 +0530

    cors issue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+var cors = require('cors')
 const app = express();
-
+app.use(cors())
 app.use(express.json())
 
 const categoryRouter = require('./routers/category')
